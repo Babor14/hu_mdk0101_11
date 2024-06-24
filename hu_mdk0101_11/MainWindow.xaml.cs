9c84dea24c60b7a30b526a47c3d10a00c97037e0
user9 <user9@mail.ru>
2024-06-24 15:27:51 +0200

    Добавьте файлы проекта.

---
+++ hu_mdk0101_11/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Linq;
+
+namespace hu_mdk0101_11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string filepath = "films.txt";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void film_click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(filepath))
+                {
+                string fileContent = File.ReadAllText(filepath);
+                Filmstextbox.Text = fileContent;
+                }
+            else
+            {
+                MessageBox.Show("файл не найден.");
+            }
+        }
+
+        private void showfilms_click(object sender, RoutedEventArgs e)
+        {
+            if(File.Exists(filepath))
+            {
+                string[] lines= File.ReadAllLines(filepath);
+                var filmsbydirector =lines.Where(Line => Line.Contains("Гофман")).ToList();
+                directfilmtextbox.Text = string.Join(Environment.NewLine, filmsbydirector);
+            }
+            else
+            {
+                MessageBox.Show("файл не найден.");
+            }
+        }
+    }
+}
